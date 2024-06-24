e5488c0b5fd64b5f2daaf6a8e27550841b414c4a
keremmwx6666 <keremmwxgta@gmail.com>
2024-06-24 10:51:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ keepAlive();
 function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'America/New_York', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    timeZone: 'Turkey/Antalya', //https://www.zeitverschiebung.net/en/ and find your city and enter here
     hour12: true,
     hour: 'numeric',
     minute: 'numeric'
@@ -27,10 +27,10 @@ client.on('ready', async () => {
     .setType('STREAMING')
     .setURL('https://www.youtube.com/watch?v=zqLMEulWcbQ') //Must be a youtube video link 
     .setState('✧˚ · . ᴀᴄᴛɪᴠᴇ 𝟸𝟺/𝟽')
-    .setName(' ')
+    .setName(' `≡;- ꒰ °ғᴜʀɪɴᴀ ꒱  ')
     .setDetails(`≡;- ꒰ °ғᴜʀɪɴᴀ ꒱ [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1100708631311286342/1253255499621597195/544a2751c85f75739ce8fc7b66393951.png?ex=6679cde3&is=66787c63&hm=d1aba233b663877200eb15027fd6029e740bea0061d107828a97d39b86edb3d5&') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1097132368441122879/1254704772406313030/544a2751c85f75739ce8fc7b66393951.png?ex=667a7661&is=667924e1&hm=2293c7716c3896f9be4b0fb439f072616b4ca1fd67104311805f6cfca5144e04&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('*:･ﾟ✧*:･ﾟ ᴋᴇʀᴇᴍᴍ 💓') //Text when you hover the Large image
     .setAssetsSmallImage('') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('') //Text when you hover the Small image
